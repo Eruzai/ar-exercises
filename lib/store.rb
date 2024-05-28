ea19b08cf5d4ac38859a1892c6abd3dd4eaca0f9
Kevin Levasseur <kevin_levasseur@telus.net>
2024-05-28 15:32:15 -0700

    feat: add validation

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_have_either_mens_or_womens_apparel
+
+  def must_have_either_mens_or_womens_apparel
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:mens_apparel, "or womens_apparel must be true")
+    end
+  end
 end
