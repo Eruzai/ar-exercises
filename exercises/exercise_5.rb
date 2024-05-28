d36456174f9dc4bb5e77b77703102845a6817e1d
Kevin Levasseur <kevin_levasseur@telus.net>
2024-05-28 13:51:10 -0700

    feat: display total and average revenue and number of stores making more than 1M

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@total_revenue = Store.sum("annual_revenue")
+puts "Stores total revenue: #{@total_revenue}"
+
+@average_revenue = Store.average("annual_revenue")
+puts "Stores average revenue: #{@average_revenue}"
+
+@count_revenue_over_1M = Store.where("annual_revenue > ?", 1000000).count
+puts "Stores generating more than 1M in revenue: #{@count_revenue_over_1M}"
