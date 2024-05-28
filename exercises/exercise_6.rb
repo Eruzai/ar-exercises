69b035af3a360c13c8540c418469d5ef32a906ad
Kevin Levasseur <kevin_levasseur@telus.net>
2024-05-28 14:05:45 -0700

    feat: add employees to some stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,9 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jacob", last_name: "Wattman", hourly_rate: 65)
+@store1.employees.create(first_name: "Henry", last_name: "Jackson", hourly_rate: 53)
+@store2.employees.create(first_name: "Carly", last_name: "Purim", hourly_rate: 72)
+@store2.employees.create(first_name: "Krysta", last_name: "Katnis", hourly_rate: 59)
+@store2.employees.create(first_name: "Serina", last_name: "Nornima", hourly_rate: 68)
