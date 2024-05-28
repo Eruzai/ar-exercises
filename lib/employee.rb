0bb37c1eb376e743573373c0b8c6d8ad3644ba5b
Kevin Levasseur <kevin_levasseur@telus.net>
2024-05-28 16:45:47 -0700

    feat: add callback to set password on employee creation

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 class Employee < ActiveRecord::Base
   belongs_to :store
 
@@ -5,4 +7,15 @@ class Employee < ActiveRecord::Base
   validates :last_name, presence: true
   validates :hourly_rate, numericality: { only_integer: true, in: 40..200 }
   validates :store_id, presence: true
+
+  # before_create :set_password
+  # after_create :set_password # requires saving after!
+  before_save :set_password # could overwrite a password given by user
+
+  private
+
+  def set_password
+    self.password = SecureRandom.base64(6)
+    # self.save # required with after_create
+  end
 end
